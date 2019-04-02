08916fda17be12e72a6b58efa847edde14fa1a5f
mvarbanov97 <momchil.varbanov97@gmail.com>
2019-04-02 17:58:56 +0300

    TODO-List-Tech-Module-CSharp
    starting the TODO project

---
+++ Controllers/Data/ToDoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=ToDoListDb;Integrated Security=True;";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+    }
+}
